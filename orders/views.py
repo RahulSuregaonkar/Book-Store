18b886cd7bdf7d09472d0026e1029c98f359e51c
rahul suregaonkar <rahulsuregaonkar@gmai.com>
2024-01-17 11:46:42 +0530

    _documentation update

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.decorators import login_required
 from basket.basket import Basket
 from account.models import Address
 from .models import Order, OrderItem,ReturnPolicy
+from django.core.mail import send_mail
+from django.contrib import messages
+
+
+
 
 
 
@@ -12,13 +17,11 @@ def add(request):
     basket = Basket(request)
     if request.POST.get("action") == "post":
 
-        
         order_key = request.POST.get("order_key")
         user_id = request.user.id
         baskettotal = basket.get_total_price()
         full_name1 = str(request.user.name)
         mobile = str(request.user.mobile)
-        
 
         # Check if order exists
         if Order.objects.filter(order_key=order_key).exists():
@@ -27,17 +30,40 @@ def add(request):
             order = Order.objects.create(
                 user_id=user_id,
                 full_name=full_name1,
-                phone = mobile,
+                phone=mobile,
                 total_paid=baskettotal,
                 order_key=order_key,
-                order_status = "Received"
+                order_status="Received"
             )
             order_id = order.pk
+            product_details = ""
 
             for item in basket:
                 OrderItem.objects.create(
                     order_id=order_id, product=item["product"], price=item["price"], quantity=item["qty"]
                 )
+
+                # Include product details in the email message
+                product_details += f"<tr><td>{item['product']}</td><td>{item['qty']}</td><td>₹{item['price']}</td></tr>"
+
+        # Create an HTML table for the product details
+        product_table = f"<table><tr><th>Product</th><th>Quantity</th><th>Price</th></tr>{product_details}</table>"
+
+        order_details_message = f"""<p>Order #{order_id} successfully placed for {full_name1}.</p>
+        <p>Total amount paid: ₹{baskettotal}</p>
+        {product_table}"""
+
+        send_mail(
+            'Order Confirmation',
+            order_details_message,
+            'rahulsuregaonkar@gmail.com',  # Change to your email address
+            [request.user.email],  # Send to the user's email address
+            fail_silently=False,
+            html_message=order_details_message,  # Specify the HTML content
+        )
+
+        # Use messages framework to store the message
+        messages.success(request, "Order placed successfully!")
 
         response = JsonResponse({"success": "Return something"})
         return response
